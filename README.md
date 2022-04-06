7819eafa589f68e4fdcbff33a360bd1cb0613fef
jochen4errors <j4e@mailbox.org>
2022-04-06 19:05:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# projektverwaltung
+
+---
+
+Workflow: Anlegen eines Projekts mit dem Editor Geany und der Versionskontrolle Git
+
+---
+
+by jochen4errors, started at 2022-04-06.
+
+---
---
+++ test.sh
@@ -0,0 +1,12 @@
+start=#!/bin/bash
+
+echo "=== projektverwaltung ==="
+echo "Workflow: Anlegen eines Projekts mit dem Editor Geany und der Versionskontrolle Git"
+echo "© by jochen4errors, see LICENSE-file."
+echo "Version: 1.0 - 06.04.2022 18:38:12 CEST"
+echo ""
+
+
+
+echo "=== end ==="
+read -p "⏎ "
